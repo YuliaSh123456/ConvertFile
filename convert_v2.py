44119b7d33f7aa202474cf5d6fc25ffd8c1e7b88
Yulia Sheynkman <Yulias@Kayhut.local>
2020-10-13 06:11:29 -0700

    initial version

--- convert_v2.py
+++ convert_v2.py
@@ -4,8 +4,10 @@ import gzip
 import shutil
 import tarfile
 import rarfile
+import patoolib
+import unrar
 
-class clFilesFamily:
+class FilesFamily:
 
     def __init__(self, list_of_formats):
         """
@@ -15,13 +17,24 @@ class clFilesFamily:
         self.list_of_formats = list_of_formats
 
 
-    def AddFormats(self, list_of_formats):
+    def add_formats(self, list_of_formats):
         self.list_of_formats = list_of_formats
 
-    def AddFormat(self, str_format_name):
+    def add_format(self, str_format_name):
         self.list_of_formats.append(str_format_name)
 
-    def Convert(self, file_path, dest_format):
+    def _create_tar_family_archive(self, file_path, suffix_str, w_mode):
+
+        tgz_path = file_path.split(".")[0] + suffix_str
+        out_dir = file_path.split(".")[0]
+        out_dir = out_dir.split("\\")
+        out_dir = out_dir[len(out_dir) - 1] + "\\" + out_dir[len(out_dir) - 1] + suffix_str
+
+        with tarfile.open(file_path.split(".")[0] + suffix_str, mode=w_mode) as tar:
+            tar.add(file_path.split(".")[0] + suffix_str, arcname=out_dir)
+
+
+    def convert(self, file_path, dest_format):
         print '{} {}'.format('Convert to format', dest_format)
 
         len_l = len(file_path.split("."))
@@ -41,36 +54,19 @@ class clFilesFamily:
             fp.close()
 
         if (suffix_of_src_file == "rar"):
-            fp = rarfile.open(file_path, "r:gz")
-            fp.extractall()
-            fp.close()
+            patoolib.extract_archive(file_path)
 
         if (dest_format == "tar"):
-            suffix_str = ".tar"
-            w_mode = "w"
+            self._create_tar_family_archive(file_path, ".tar", "w")
 
         if (dest_format == "gz"):
-            suffix_str = ".tar.gz"
-            w_mode = "w:gz"
+            self._create_tar_family_archive(file_path, ".tar.gz", "w:gz")
 
         if (dest_format == "rar"):
-            suffix_str = ".tar.gz"
-            w_mode = "w:gz"
-
-        tgz_path = file_path.split(".")[0] + suffix_str
-        out_dir = file_path.split(".")[0]
-        out_dir = out_dir.split("\\")
-        out_dir = out_dir[len(out_dir) - 1] + "\\" + out_dir[len(out_dir) - 1] + suffix_str
-
-       # with tarfile.open(file_path.split(".")[0]+".tar.gz", mode="w:gz") as tar:
-       #     tar.add(file_path.split(".")[0] + ".tar.gz", arcname='backup\\backup.tar.gz')
-        with tarfile.open(file_path.split(".")[0] + suffix_str, mode=w_mode) as tar:
-            tar.add(file_path.split(".")[0] + suffix_str, arcname=out_dir)
-
-        #if (dest_format == "rar"):
+            patoolib.create_archive(file_path.split(".")[0]+".rar", (file_path.split(".")[0],))
 
 
-    def IsFormatCompatible(self, format_name):
+    def is_format_compatible(self, format_name):
         if ( format_name in self.list_of_formats):
             return True
         else:
@@ -81,30 +77,31 @@ def build_parser():
     parser.add_argument('src_file', action="store", type=str)
     parser.add_argument('format_name', action="store", type=str)
 
-    parse_results = parser.parse_args()
+    return parser
 
-    return parse_results
+
 
 
 def main():
-    parser = build_parser()
+    options = build_parser().parse_args()
+
     lst_of_txt_formats = ["txt", "pdf", "doc"]
     lst_of_zip_formats = ["gz", "rar", "tar", "zip"]
 
-    txtFamily = clFilesFamily(lst_of_txt_formats)
-    zipFamily = clFilesFamily(lst_of_zip_formats)
+    txtFamily = FilesFamily(lst_of_txt_formats)
+    zipFamily = FilesFamily(lst_of_zip_formats)
 
     format_to_class_dict = {"rar":zipFamily, "gz":zipFamily, "tar":zipFamily, "txt":txtFamily}
     working_class = None
 
-    if (parser.format_name in format_to_class_dict.keys()):
-        working_class = format_to_class_dict[parser.format_name]
+    if (options.format_name in format_to_class_dict.keys()):
+        working_class = format_to_class_dict[options.format_name]
     else:
         print ("Incorrect format")
         exit
 
-    if (working_class.IsFormatCompatible(parser.format_name)):
-        working_class.Convert(parser.src_file, parser.format_name)
+    if (working_class.is_format_compatible(options.format_name)):
+        working_class.convert(options.src_file, options.format_name)
     else:
         print("Incompatible format")
 
