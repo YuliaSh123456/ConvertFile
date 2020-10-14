31b224eced9707badedb4231b3a57edd03ac3b5a
Yulia Sheynkman <Yulias@Kayhut.local>
2020-10-14 03:44:36 -0700

    fixed 3

--- convert_file.py
+++ convert_file.py
@@ -1,9 +1,19 @@
+import os
 import sys
 import argparse
 import gzip
 import shutil
 import tarfile
 import patoolib
+
+
+def _create_tar_family_archive(file_path, suffix_str, w_mode):
+    out_dir = file_path.split(".")[0]
+    out_dir = out_dir.split("\\")
+    out_dir = out_dir[len(out_dir) - 1] + "\\" + out_dir[len(out_dir) - 1] + suffix_str
+
+    with tarfile.open(file_path.split(".")[0] + suffix_str, mode=w_mode) as tar:
+        tar.add(file_path.split(".")[0] + suffix_str, arcname=out_dir)
 
 
 class FilesFamily(object):
@@ -17,21 +27,10 @@ class FilesFamily(object):
 
         self.list_of_formats = list_of_formats
 
-    def _create_tar_family_archive(self, file_path, suffix_str, w_mode):
-        tgz_path = file_path.split(".")[0] + suffix_str
-        out_dir = file_path.split(".")[0]
-        out_dir = out_dir.split("\\")
-        out_dir = out_dir[len(out_dir) - 1] + "\\" + out_dir[len(out_dir) - 1] + suffix_str
-
-        with tarfile.open(file_path.split(".")[0] + suffix_str, mode=w_mode) as tar:
-            tar.add(file_path.split(".")[0] + suffix_str, arcname=out_dir)
-
-
     def convert(self, file_path, dest_format):
         print 'Convert to format {}'.format(dest_format)
 
-        len_l = len(file_path.split("."))
-        suffix_of_src_file = file_path.split(".")[len_l-1]
+        suffix_of_src_file = file_path.split(".")[-1]
 
         if suffix_of_src_file not in self.list_of_formats:
             print ("The file is not part of the family")
@@ -45,16 +44,17 @@ class FilesFamily(object):
             patoolib.extract_archive(file_path)
 
         if dest_format == "tar":
-            self._create_tar_family_archive(file_path, ".tar", "w")
+            _create_tar_family_archive(file_path, ".tar", "w")
 
         if dest_format == "gz":
-            self._create_tar_family_archive(file_path, ".tar.gz", "w:gz")
+            _create_tar_family_archive(file_path, ".tar.gz", "w:gz")
 
         if dest_format == "rar":
-            patoolib.create_archive(file_path.split(".")[0]+".rar", (file_path.split(".")[0],))
+            patoolib.create_archive(file_path.split(".")[0] + ".rar", (file_path.split(".")[0],))
 
     def is_format_compatible(self, format_name):
         return format_name in self.list_of_formats
+
 
 def build_parser():
     parser = argparse.ArgumentParser(description='This is a File converter')
@@ -63,29 +63,28 @@ def build_parser():
 
     return parser
 
+
 def main():
     options = build_parser().parse_args()
 
-    lst_of_txt_formats = ["txt", "pdf", "doc"]
     lst_of_zip_formats = ["gz", "rar", "tar", "zip"]
 
-    txtFamily = FilesFamily(lst_of_txt_formats)
-    zipFamily = FilesFamily(lst_of_zip_formats)
+    zip_family = FilesFamily(lst_of_zip_formats)
 
-    format_to_class_dict = {"rar":zipFamily, "gz":zipFamily, "tar":zipFamily, "txt":txtFamily}
+    format_to_class_dict = {"rar": zip_family, "gz": zip_family, "tar": zip_family}
     working_class = None
 
     if options.format_name in format_to_class_dict.keys():
         working_class = format_to_class_dict[options.format_name]
     else:
-        print ("Incorrect format")
+        raise RuntimeError("Incorrect format")
         exit
 
     if working_class.is_format_compatible(options.format_name):
         working_class.convert(options.src_file, options.format_name)
     else:
-        print("Incompatible format")
+        raise RuntimeError("Incompatible format")
+
 
 if __name__ == "__main__":
     main()
-
